31bb7300d6d1f8ae9e64365c0ff5e1014a2dce45
madmanor <97254556+madmanor@users.noreply.github.com>
2024-06-17 15:32:52 -0500

    Edit README.md and hello_world.txt

---
+++ joseph.java
@@ -0,0 +1,14 @@
+public class joseph {
+
+    string joe = "Jacob";
+    boolean isGay = true;
+
+    if (isGay == true) {
+        System.out.println ("Jacob is a faggot!!!");
+
+    }
+    else {
+        System.out.println ("Jacob is not a faggot!!!");
+    }
+
+} 
